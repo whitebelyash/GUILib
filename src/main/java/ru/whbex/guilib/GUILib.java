b8d5ccabea2713f033205ce53c59d5594b870972
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2023-06-09 19:37:06 +0300

    remove tests

--- src/main/java/ru/whbex/guilib/GUILib.java
+++ src/main/java/ru/whbex/guilib/GUILib.java
@@ -2,38 +2,27 @@ package ru.whbex.guilib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.whbex.guilib.gui.GUIManager;
-import ru.whbex.guilib.tests.GuiInfoCommand;
-import ru.whbex.guilib.tests.TestCommand;
 
 import java.util.logging.Logger;
 
 public final class GUILib extends JavaPlugin {
     public static Logger LOGGER;
     private static GUILib instance;
-    private GUIManager gm;
 
     @Override
     public void onEnable() {
         LOGGER = this.getLogger();
         instance = this;
         getLogger().info("Startup!");
-        gm = new GUIManager(this, null, true);
-        gm.bindClick(-999, ((player, context) -> context.guiManager().close(player)));
-        this.getCommand("guitesting").setExecutor(new TestCommand());
-        this.getCommand("guiinfo").setExecutor(new GuiInfoCommand());
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 
     public static GUILib getInstance() {
         return instance;
-    }
-
-    public GUIManager getGUIManager() {
-        return gm;
     }
 }
