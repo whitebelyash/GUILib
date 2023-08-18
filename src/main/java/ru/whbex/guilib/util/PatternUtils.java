141f89c258d889775d2f488c334b21d4f67f9cf2
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2023-08-18 12:40:04 +0300

    strip debug logging in PatternUtils

--- src/main/java/ru/whbex/guilib/util/PatternUtils.java
+++ src/main/java/ru/whbex/guilib/util/PatternUtils.java
@@ -15,17 +15,14 @@ public class PatternUtils {
     }
     public static String validatePatternLine(String input){
         StringBuilder output = new StringBuilder(input);
-        GUILib.LOGGER.info("Validator: size " + output.length());
         int outputLen = output.length();
         if(outputLen < 9){
             for(int c = 0; c < 9 - outputLen; c++){
-                GUILib.LOGGER.info("Validator: c " + c + " output len " + outputLen + " 9 - output " + (9 - output.length()));
                 output.append(Pattern.EMPTY_CHAR);
             }
         } if(outputLen > 9){
             output.setLength(9);
         }
-        GUILib.LOGGER.info("Validator after: size " + output.length());
         return output.toString();
     }
 }
