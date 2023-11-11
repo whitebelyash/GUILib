1ac22b7b1a25985cfa9d6b40d4e5044a8a6ad251
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2023-11-12 03:17:39 +0400

    some info for CrossGUIContext

--- src/main/java/ru/whbex/guilib/gui/misc/CrossGUIContext.java
+++ src/main/java/ru/whbex/guilib/gui/misc/CrossGUIContext.java
@@ -2,7 +2,15 @@ package ru.whbex.guilib.gui.misc;
 
 
 import ru.whbex.guilib.gui.GUIInstance;
-
+/**
+    How to Use:
+    1. Extend this class
+    2. Add your methods, fields, etc
+    3. create cross context instance e.g. before opening new GUI
+    3. call GUIInstance#setCrossContext() anywhere you want
+    4. ????
+    5. TBD
+ */
 public abstract class CrossGUIContext {
     protected final GUIInstance guiInstance;
     public CrossGUIContext(GUIInstance gi){
